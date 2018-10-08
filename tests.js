1156ffeabea008027fb5aa5d350ce7223e470f65
antonioortegajr <antonioortegajr@gmail.com>
2018-10-08 10:33:50 -0700

    Create tests.js

---
+++ tests.js
@@ -0,0 +1,24 @@
+/* Status code tests
+*  Various test against the returned status code.
+*/
+
+// Check for the status code is equal to 200 and 200 only
+pm.test("Status code is 200", function () {
+    pm.response.to.have.status(200);
+});
+
+// test returned status code is 200 or you can add multiple values
+pm.test("Successful POST request", function () {
+    pm.expect(pm.response.code).to.be.oneOf([200]);
+});
+
+
+/* Variables
+*  Set or unset variables
+*/
+
+// set the API call response body to a variable after parsing
+var returnBody = JSON.parse(responseBody);
+
+// Set variable for the current envirnment
+pm.environment.set("EnvirnmentVariable", "exampleValue");
